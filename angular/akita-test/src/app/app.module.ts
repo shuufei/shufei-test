7adb7536a7b45ca493b1f6fe9f1991c1c069c6da
shuufei <digital.shuufei@gmail.com>
2020-10-23 20:18:46 +0900

    add akita

--- angular/akita-test/src/app/app.module.ts
+++ angular/akita-test/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    AkitaNgRouterStoreModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
