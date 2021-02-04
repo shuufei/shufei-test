134794915e5c2b98bae0e96bed2c943a1fce6823
shuufei <digital.shuufei@gmail.com>
2021-02-04 11:48:47 +0900

    amplifyでapiを構築テスト

--- graphql/amplify-fargate-test/src/polyfills.ts
+++ graphql/amplify-fargate-test/src/polyfills.ts
@@ -61,3 +61,11 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/**
+ * for amplify
+ */
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
