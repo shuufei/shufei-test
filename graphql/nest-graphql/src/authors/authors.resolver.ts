c09a2ca0e94c670e52cc6cc58a87064291410139
shuufei <digital.shuufei@gmail.com>
2021-02-12 00:26:03 +0900

    nest graphql tutorial

--- graphql/nest-graphql/src/authors/authors.resolver.ts
+++ graphql/nest-graphql/src/authors/authors.resolver.ts
@@ -1,16 +1,98 @@
 // import { Query } from "@nestjs/common";
-import { Args, Int, Resolver, Query } from "@nestjs/graphql";
-import { Author } from './models/author.models';
+import { Args, Int, Resolver, Query, Mutation, ArgsType, Field, InputType, ResolveField, Parent } from "@nestjs/graphql";
+import { Post } from "src/posts/models/post.models";
+import { Author, CreateAuthorInput, UpdateAuthorInput } from './models/author.models';
+
+// @ArgsType()
+@InputType()
+class UpvotePostArgs {
+  @Field(returns => Int)
+  postId: number;
+
+  @Field({nullable: true})
+  title: string;
+}
 
 @Resolver(of => Author)
 export class AuthorsResolver {
+  @Query(returns => [Author])
+  async authors(): Promise<Author[]> {
+    return [
+      {
+        id: 99,
+        posts: []
+      },
+      {
+        id: 88,
+        posts: []
+      },
+      {
+        id: 77,
+        posts: []
+      },
+    ]
+  }
+
   @Query(returns => Author)
-  async author(@Args('id', { type: () => Int }) id: number) {
+  async author(@Args('id', { type: () => Int }) id: number): Promise<Author> {
     return {
       id,
       posts: []
     };
   }
 
+  @ResolveField('posts', returns => [Post])
+  async getPosts(@Parent() author: Author): Promise<Post[]> {
+    console.log('--- get posts: ', author);
+    const { id } = author;
+    // return this.postsService.findAll({ authorId: id });
+    return [
+      {
+        id: 11,
+        title: 'post-11'
+      },
+      {
+        id: 22,
+        title: 'post-22'
+      },
+      {
+        id: 33,
+        title: 'post-33'
+      }
+    ];
+  }
+
+  @Mutation(returns => Author)
+  async createAuthor(@Args('params') params: CreateAuthorInput): Promise<Author> {
+    console.log('--- create params: ', params);
+    return {
+      id: 99,
+      firstName: params.firstName,
+      lastName: params.lastName,
+      posts: []
+    };
+  }
+
+  @Mutation(returns => Author)
+  async updateAuthor(@Args('params') params: UpdateAuthorInput): Promise<Author> {
+    console.log('--- update params: ', params);
+    return {
+      id: 88,
+      firstName: params.firstName,
+      lastName: params.lastName,
+      posts: []
+    }
+  }
+
+  @Mutation(returns => Post)
+  async upvotePost(@Args('upvotePostData') args: UpvotePostArgs): Promise<Post> {
+    // return this.postsService.upvoteById({ id: postId });
+    console.log('---- postid: ', args.postId);
+    return {
+      id: 0,
+      title: 'ttiel',
+      votes: 1
+    };
+  }
 
 }
--- graphql/nest-graphql/src/authors/models/author.models.ts
+++ graphql/nest-graphql/src/authors/models/author.models.ts
@@ -1,9 +1,30 @@
-import { Field, ObjectType, Int } from '@nestjs/graphql';
-import { Post } from '../../posts/models/post.module';
+import { Field, ObjectType, Int, registerEnumType, InputType, PartialType, OmitType, PickType } from '@nestjs/graphql';
+import { Post } from '../../posts/models/post.models';
+import { FieldMiddleware, MiddlewareContext, NextFn } from '@nestjs/graphql';
+
+const loggerMiddleware: FieldMiddleware = async (
+  ctx: MiddlewareContext,
+  next: NextFn,
+) => {
+  const value = await next();
+  console.log(value);
+  return value;
+};
+
+export enum RoleType {
+  System,
+  Company,
+}
+
+registerEnumType(RoleType, {
+  name: 'RoleType'
+})
 
 @ObjectType()
 export class Author {
-  @Field(type => Int)
+  @Field(type => Int, {
+    middleware: [loggerMiddleware]
+  })
   id: number;
 
   @Field({ nullable: true })
@@ -12,6 +33,21 @@ export class Author {
   @Field({ nullable: true })
   lastName?: string;
 
-  @Field(type => [Post])
+  @Field(type => [Post], {nullable: 'itemsAndList'})
   posts: Post[];
+
+  @Field(types => RoleType, {nullable: true})
+  roleType?: RoleType
 }
+
+@InputType()
+export class CreateAuthorInput {
+  @Field({nullable: true})
+  firstName?: string;
+
+  @Field({nullable: true})
+  lastName?: string;
+}
+
+@InputType()
+export class UpdateAuthorInput extends CreateAuthorInput {}
