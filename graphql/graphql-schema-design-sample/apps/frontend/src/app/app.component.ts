b57f9b9010917f6bad0b252d8282c6cc809f1382
shuufei <digital.shuufei@gmail.com>
2021-03-21 20:03:47 +0900

    generate angular app

---
+++ graphql/graphql-schema-design-sample/apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'graphql-schema-design-sample-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
